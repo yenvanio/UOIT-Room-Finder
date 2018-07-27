383eace57eea8b17b405555e4637c3fb7e0b95fe
Shivanouyen Yoganathan <shivanouyen.yoganathan@uoit.net>
2018-07-27 12:56:04 -0400

    update url

--- UOIT-Room-Finder-API/app.js
+++ UOIT-Room-Finder-API/app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 var classController = require('./api/controllers/classes');
-app.use('/class', classController);
+app.use('/api/class', classController);
 
 var roomController = require('./api/controllers/rooms');
-app.use('/room', roomController);
+app.use('/api/room', roomController);
 
 module.exports = app;
